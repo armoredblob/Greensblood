697e6aedd802607a7922fdfea8a795920d93667f
Armored Blob <nico.goldreyer@gmail.com>
2020-04-22 10:23:38 -0400

    CamMovement script
    A C# script detailing camera movement.

---
+++ BasicMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform mainCamera;
+    CharacterController controller;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Vector3 moveDirection;
+        moveDirection = Vector3.zero;
+        if (Input.GetKey("w"))
+        {
+            moveDirection += mainCamera.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            moveDirection -= mainCamera.forward;
+        }
+        if (Input.GetKey("d"))
+        {
+            moveDirection += mainCamera.right;
+        }
+        if (Input.GetKey("a"))
+        {
+            moveDirection -= mainCamera.right;
+        }
+    
+
+        moveDirection.y = 0;
+        moveDirection.Normalize();
+        controller.SimpleMove(moveDirection * speed);
+    }
+}
---
+++ CamMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CamMovement: MonoBehaviour
+{
+    public Transform player;
+    public float rotateAmount;
+    float lookX, lookY;
+    void Update()
+    {
+        lookX = Input.GetAxis("Mouse X");
+        lookY = Input.GetAxis("Mouse Y");
+        
+        if (lookX > 0)
+        {
+            transform.RotateAround(player.position, Vector3.up, rotateAmount);
+            
+        }
+      
+        else if (lookX < 0)
+        {
+            transform.RotateAround(player.position, Vector3.left, rotateAmount);
+        }
+            
+            if (lookY > 0)
+        {
+            transform.RotateAround(player.position, transform.right, rotateAmount);
+            
+        }
+      
+        else if (lookY < 0)
+        {
+            transform.RotateAround(player.position, transform.right, -rotateAmount);
+        }
+        
+        transform.LookAt(player);
+    }
+}
